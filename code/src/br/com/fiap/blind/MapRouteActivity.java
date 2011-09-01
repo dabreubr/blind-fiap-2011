90283fc0dbcfdb80c82056a1d031031c107817c4
daniel.snipher@gmail.com <daniel.snipher@gmail.com@57079b9a-87e6-f1c0-dabe-6aadfb277aba>
2011-09-01 01:11:33 +0000

    Alteração para todas as activities rodarem sempre em posição retrato Alteração dos xml de layout para manterem a tela sempre ligada Reconhecimento de voz, aumento da vibração para dar tempo de abrir a pesquisa Copia do código do simuladorBlind para o projeto Blind

--- code/src/br/com/fiap/blind/MapRouteActivity.java
+++ code/src/br/com/fiap/blind/MapRouteActivity.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -17,58 +18,80 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.RectF;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
-public class MapRouteActivity extends MapActivity implements Runnable, LocationListener {
+public class MapRouteActivity extends MapActivity implements Runnable, LocationListener, OnInitListener {
 	
-	private Handler handler = new Handler();
+	private int indiceOnibus;
 	private ProgressDialog dialog;
 	private static final String URL = "http://goomedia.com.br/sptrans/ServicoRotas.asmx";
 	private TextView txtRota, txtDescription;
 	private MapView mapView;
-	private Rota mRota; 
+	private Rota mRota;
+	private String enderecoFinal;
+	private boolean trocouOnibus, chegouAoPonto, aguardarOnibus, noOnibus, chegouAoDestino, obteveRotaOnibus;
+	private Transporte transporte;
+	private ArrayList<Transporte> transportes = new ArrayList<Transporte>();
+	private TextToSpeech mTts;
+	private ReentrantLock waitForInitLock = new ReentrantLock();
+	private boolean ttsInitialized = false;
+	private Geocoder geocoder;
+	private List<Address> listAddressDesembarque = null;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mapa);
-        
-        txtRota = (TextView) findViewById(R.id.rota);
-        txtDescription = (TextView) findViewById(R.id.description);
-        mapView = (MapView) findViewById(R.id.mapview);
-        mapView.setBuiltInZoomControls(true);
-        
+        // Initialize text-to-speech. This is an asynchronous operation.
+		// The OnInitListener (second argument) is called after initialization completes.
+		mTts = new TextToSpeech(this,
+				this  // TextToSpeech.OnInitListener
+		);
+		//don't do speak until initing 
+		waitForInitLock.lock(); 
     }
 	
     @Override
     public void onStart() {
     	super.onStart();
-    	
-        getLocationManager().requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-		getLocationManager().requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-		
-		Location loc = getLocationManager().getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		if (loc != null) {
-			setNewLocation(loc);
-		}
-		
-    	dialog = ProgressDialog.show(this, "Aguarde", "Calculando rota");
-    	
-    	new Thread(this).start();
-    	
+    	iniciar();
+    }
+    
+    public void iniciar() {
+    	if (ttsInitialized) {
+            setContentView(R.layout.mapa);
+            txtRota = (TextView) findViewById(R.id.rota);
+            txtDescription = (TextView) findViewById(R.id.description);
+            mapView = (MapView) findViewById(R.id.mapview);
+            mapView.setBuiltInZoomControls(true);
+			geocoder = new Geocoder(this);
+			trocouOnibus = true;
+			aguardarOnibus = false;
+			obteveRotaOnibus = false;
+    		getLocationManager().requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+    		getLocationManager().requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    		Location loc = getLocationManager().getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    		if (loc != null) {
+    			setNewLocation(loc);
+    		}
+
+    		dialog = ProgressDialog.show(this, "Aguarde", "Calculando rota");
+
+    		new Thread(this).start();
+    	}
     }
     
     @Override
@@ -88,27 +111,7 @@ public class MapRouteActivity extends MapActivity implements Runnable, LocationL
 		try {
 			SPTransWS sptrans = new SPTransWS(URL);
 			final String rota = sptrans.calcularRota(Gps.getStrEnderecoOrigem(), Gps.getStrEnderecoDestino());
-			handler.post(new Runnable() {
-				@Override
-				public void run() {
-					double fromLat = Gps.getEnderecoOrigem().getLatitude(), 
-					fromLon = Gps.getEnderecoOrigem().getLongitude(), 
-					toLat = Gps.getEnderecoDestino().getLatitude(), 
-					toLon = Gps.getEnderecoDestino().getLongitude();
-					String url = GerarRota
-					.getUrl(fromLat, fromLon, toLat, toLon);
-					InputStream is = getConnection(url);
-					mRota = GerarRota.getRoute(is);
-					txtDescription.setText(mRota.mName + " " + mRota.mDescription);
-					RouteOverlay routeOverlay = new RouteOverlay(mRota, mapView);
-					List<Overlay> listOfOverlays = mapView.getOverlays();
-					listOfOverlays.clear();
-					listOfOverlays.add(routeOverlay);
-					mapView.invalidate();
-					tratarResposta(rota);
-				}
-				
-			});
+			tratarResposta(rota);		
 		} catch (Exception e) {
 			Log.e("Rota", e.getMessage(), e);
 		} finally {
@@ -120,8 +123,7 @@ public class MapRouteActivity extends MapActivity implements Runnable, LocationL
 		Integer quantidadeConducao = new Integer(rota.substring(rota.indexOf("quantidadeConducao=") + 19, rota.indexOf("quantidadeConducao=") + 20));
 		String tempoViagem = rota.substring(rota.indexOf("tempoViagem=") + 12, rota.indexOf("tempoViagem=") + 17);
 		String valorTotalTarifa = rota.substring(rota.indexOf("valorTotalTarifa=") + 17, rota.indexOf("valorTotalTarifa=") + 24);
-		String enderecoFinal = tratarTexto(rota.substring(rota.indexOf("enderecoFinal=") + 14, rota.indexOf("; }")));
-		ArrayList<Transporte> transportes = new ArrayList<Transporte>();
+		enderecoFinal = tratarTexto(rota.substring(rota.indexOf("enderecoFinal=") + 14, rota.indexOf("; }")));
 		Integer i=1;
 		while (quantidadeConducao >= i) {
 			Transporte transporte = new Transporte("Ônibus");
@@ -131,17 +133,7 @@ public class MapRouteActivity extends MapActivity implements Runnable, LocationL
 			transportes.add(transporte);
 			i++;
 		}
-		
-		rota = "\n origem: " + Gps.getStrEnderecoOrigem();
-		rota += "\n destino: " + Gps.getStrEnderecoDestino();
-		/*rota += "\n quantidadeConducao: " + quantidadeConducao;
-		rota += "\n tempoViagem: " + tempoViagem;
-		rota += "\n valorTotalTarifa: " + valorTotalTarifa;
-		rota += "\n enderecoFinal: " + enderecoFinal;
-		rota += "\n transportes: " + transportes;*/
-		
-		txtRota.setText("Rota: " + rota);
-		txtRota.setVisibility(View.VISIBLE);
+		obteveRotaOnibus = true;
 	}
 
 	private String tratarTexto(String texto) {
@@ -157,6 +149,27 @@ public class MapRouteActivity extends MapActivity implements Runnable, LocationL
 		texto = texto.replace("ESTR. ", "ESTRADA ");
 		texto = texto.replace("GEN. ", "GENERAL ");
 		return texto;
+	}
+	
+	public void falar(String fala) {
+		fala = tratarTexto(fala);
+		txtDescription.setText(fala);
+		fala = fala.replace("\n", "");
+		mTts.speak(fala, TextToSpeech.QUEUE_ADD, null);
+	}
+	
+	public void obterMapaeRota(double fromLat, double fromLon, double toLat, double toLon) {
+		String url = GerarRota.getUrl(fromLat, fromLon, toLat, toLon);
+		InputStream is = getConnection(url);
+		if (is!=null) { // alterar
+			mRota = GerarRota.getRoute(is);
+			txtDescription.setText(mRota.mName + " " + mRota.mDescription);
+			RouteOverlay routeOverlay = new RouteOverlay(mRota, mapView);
+			List<Overlay> listOfOverlays = mapView.getOverlays();
+			listOfOverlays.clear();
+			listOfOverlays.add(routeOverlay);
+			mapView.invalidate();
+		}
 	}
 	
 	private InputStream getConnection(String url) {
@@ -180,21 +193,128 @@ public class MapRouteActivity extends MapActivity implements Runnable, LocationL
 	@Override
 	public void onLocationChanged(Location location) {
 		setNewLocation(location);
+		atualizaRota();
 	}
 
 	private void setNewLocation(Location location) {
 		Gps.setLatitude((Double) location.getLatitude());
 		Gps.setLongitude((Double) location.getLongitude());
-		
-		int latitude = (int) (location.getLatitude()*1E6);
-		int longitude = (int) (location.getLongitude()*1E6);
-		
-		if (mRota != null) {
-			mapView.getOverlays().clear();
-			mapView.getOverlays().add(new RouteOverlay(mRota, mapView));
-			mapView.getOverlays().add(new PositionOverlay(latitude, longitude));
-			mapView.getController().setCenter(new GeoPoint(latitude, longitude));
-			mapView.invalidate();
+	}
+	
+	private void atualizaRota() {
+		try {
+			if (obteveRotaOnibus) {
+				txtRota.setText("Origem: " + Gps.getStrEnderecoOrigem() + " Destino: " + enderecoFinal);
+				double latitude = Gps.getLatitude();
+				double longitude = Gps.getLongitude();
+				int latitudeInt = (int) (latitude*1E6);
+				int longitudeInt = (int) (longitude*1E6);
+				int latitudeAprox = (int) (latitude*1E4);
+				int longitudeAprox = (int) (longitude*1E4);
+
+				if (trocouOnibus) {
+					aguardarOnibus = false;
+					List<Address> listAddress = null;
+					if (transportes.size() > indiceOnibus) {
+						transporte = transportes.get(indiceOnibus);
+						listAddress = geocoder.getFromLocationName(transporte.getEmbarque(), 1);
+					} else {
+						listAddress = geocoder.getFromLocationName(enderecoFinal, 1);
+					}
+					double toLat = listAddress.get(0).getLatitude();
+					double toLon = listAddress.get(0).getLongitude();
+					obterMapaeRota(latitude, longitude, toLat, toLon);
+					trocouOnibus = false;
+				}	
+
+				if (mRota != null) {
+					mapView.getOverlays().clear();
+					mapView.getOverlays().add(new RouteOverlay(mRota, mapView));
+					mapView.getOverlays().add(new PositionOverlay(latitudeInt, longitudeInt));
+					mapView.getController().setCenter(new GeoPoint(latitudeInt, longitudeInt));
+					mapView.getController().setZoom(21);
+					mapView.invalidate();
+					for (int i=0; i<mRota.mPoints.length; i++) {
+						if (((int) (mRota.mPoints[i].getmLatitude()*1E4) == latitudeAprox) &&
+								((int) (mRota.mPoints[i].getmLongitude()*1E4) == longitudeAprox)) {
+							String fala = null;
+							if (mRota.mPoints[i].getmName() != null)
+								fala = mRota.mPoints[i].getmName();
+							if (mRota.mPoints[i].getmDescription() != null)
+								fala = fala + " \n" + mRota.mPoints[i].getmDescription();
+							if (fala != null) {
+								falar(fala);
+								if (fala.startsWith("Chegar em:"))
+									if (transportes.size() > indiceOnibus) 
+										chegouAoPonto = true;
+									else
+										chegouAoDestino = true;
+							}
+							break;
+						}
+					}
+				} else {
+					mapView.getOverlays().clear();
+					mapView.getOverlays().add(new PositionOverlay(latitudeInt, longitudeInt));
+					mapView.getController().setCenter(new GeoPoint(latitudeInt, longitudeInt));
+					mapView.getController().setZoom(21);
+					mapView.invalidate();
+				}
+
+				if (chegouAoPonto) {
+					String fala;
+					fala = "Você chegou ao ponto de " + transporte.getTipo() + 
+					" \nPegue o " + transporte.getTipo() + 
+					" " + transporte.getLinha();
+					falar(fala);
+					mRota = null;
+					chegouAoPonto = false;
+					aguardarOnibus = true;
+				}
+
+				if (chegouAoDestino) {
+					falar("Você chegou ao seu destino \n" + enderecoFinal);
+					mRota = null;
+					chegouAoDestino = false;
+				}
+
+				if (aguardarOnibus) {
+					// Espera onibus chegar via WebService
+					aguardarOnibus = false;
+					noOnibus = true;
+				}
+
+				if (noOnibus) {
+					if (listAddressDesembarque == null)
+						listAddressDesembarque = geocoder.getFromLocationName(transporte.getDesembarque(), 1);
+					else {
+						double toLat = listAddressDesembarque.get(0).getLatitude();
+						double toLon = listAddressDesembarque.get(0).getLongitude();
+						Location localAtual = new Location("reverseGeocoded");
+						Location localDesembarque = new Location("reverseGeocoded");
+						localAtual.setLatitude(latitude);
+						localAtual.setLongitude(longitude);
+						localDesembarque.setLatitude(toLat);
+						localDesembarque.setLongitude(toLon);
+						double distance = localAtual.distanceTo(localDesembarque);
+						if (distance <= 100.00) {
+							falar("A distância para o desembarque é de " + 
+									(int) distance + " metros.");
+							if ((int) distance <= 5) { // se distancia menor que 5 metros
+								falar("Desembarque no próximo ponto.");
+								if ((int) distance <= 2) { // se distancia menor que 5 metros
+									trocouOnibus = true;
+									indiceOnibus++;
+									noOnibus = false;
+									listAddressDesembarque = null;
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -216,21 +336,34 @@ public class MapRouteActivity extends MapActivity implements Runnable, LocationL
 		
 	}
 
+	@Override
+	public void onInit(int status) {
+		// status can be either TextToSpeech.SUCCESS or TextToSpeech.ERROR.
+		if (status == TextToSpeech.SUCCESS) {
+			//unlock it so that speech will happen 
+			waitForInitLock.unlock(); 
+			ttsInitialized = true;
+			iniciar();
+		} else {
+			// Initialization failed.
+			Log.e("DesambiguaVoz", "Could not initialize TextToSpeech.");
+		}
+	}
+
 }
 
-
 class PositionOverlay extends com.google.android.maps.Overlay {
-	
+
 	private int latitude, longitude;
 	private GeoPoint geoPoint;
 	private Paint paint = new Paint();
-	
+
 	public PositionOverlay(int latitude, int longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
 		geoPoint = new GeoPoint(this.latitude, this.longitude);
 	}
-	
+
 	@Override
 	public void draw(Canvas canvas, MapView mv, boolean shadow) {
 		super.draw(canvas, mv, shadow);
@@ -241,7 +374,7 @@ class PositionOverlay extends com.google.android.maps.Overlay {
 			RectF r = new RectF(ponto.x, ponto.y, ponto.x+bitmap.getWidth(), ponto.y+bitmap.getHeight());
 			canvas.drawBitmap(bitmap, null, r, paint);
 		}
-		
+
 	}
 }
 
@@ -257,10 +390,6 @@ class RouteOverlay extends com.google.android.maps.Overlay {
 				mPoints.add(new GeoPoint((int) (rota.mRoute[i][1] * 1000000),
 						(int) (rota.mRoute[i][0] * 1000000)));
 			}
-			
-			MapController mapController = mv.getController();
-			mapController.animateTo(Gps.getPonto());
-			mapController.setZoom(21);
 		}
 	}
 
@@ -274,19 +403,21 @@ class RouteOverlay extends com.google.android.maps.Overlay {
 	public void drawPath(MapView mv, Canvas canvas) {
 		int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
 		Paint paint = new Paint();
-		paint.setColor(Color.BLUE);
-		paint.setStyle(Paint.Style.FILL_AND_STROKE);
+		paint.setColor(Color.GREEN);
+		paint.setStyle(Paint.Style.STROKE);
 		paint.setStrokeWidth(3);
-		for (int i = 0; i < mPoints.size(); i++) {
-			Point point = new Point();
-			mv.getProjection().toPixels(mPoints.get(i), point);
-			x2 = point.x;
-			y2 = point.y;
-			if (i > 0) {
-				canvas.drawLine(x1, y1, x2, y2, paint);
+		if (mPoints != null) { // alterado colocar no blind tambem
+			for (int i = 0; i < mPoints.size(); i++) {
+				Point point = new Point();
+				mv.getProjection().toPixels(mPoints.get(i), point);
+				x2 = point.x;
+				y2 = point.y;
+				if (i > 0) {
+					canvas.drawLine(x1, y1, x2, y2, paint);
+				}
+				x1 = x2;
+				y1 = y2;
 			}
-			x1 = x2;
-			y1 = y2;
 		}
 	}
 }
--- code/src/br/com/fiap/blind/Point.java
+++ code/src/br/com/fiap/blind/Point.java
@@ -6,4 +6,35 @@ public class Point {
     String mIconUrl;
     double mLatitude;
     double mLongitude;
+    
+    public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public String getmDescription() {
+		return mDescription;
+	}
+	public void setmDescription(String mDescription) {
+		this.mDescription = mDescription;
+	}
+	public String getmIconUrl() {
+		return mIconUrl;
+	}
+	public void setmIconUrl(String mIconUrl) {
+		this.mIconUrl = mIconUrl;
+	}
+	public double getmLatitude() {
+		return mLatitude;
+	}
+	public void setmLatitude(double mLatitude) {
+		this.mLatitude = mLatitude;
+	}
+	public double getmLongitude() {
+		return mLongitude;
+	}
+	public void setmLongitude(double mLongitude) {
+		this.mLongitude = mLongitude;
+	}
 }
