f9e1c3934b45b520c9da61636842e2bafcaa6628
daniel.snipher@gmail.com <daniel.snipher@gmail.com@57079b9a-87e6-f1c0-dabe-6aadfb277aba>
2011-09-05 02:04:21 +0000

    Criada funcionalidade de falar que o onibus está chegando Criada classe PosicaoOnibus para saber o lugar em que o onibus está Alterado a classe SPTransWS, adicionado metodo de saber a posicao do onibus

--- SimuladorBlind/src/br/com/fiap/simuladorblind/SPTransWS.java
+++ SimuladorBlind/src/br/com/fiap/simuladorblind/SPTransWS.java
@@ -1,34 +1,67 @@
 package br.com.fiap.simuladorblind;
 
 import java.io.IOException;
-
 import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.Marshal;
+import org.ksoap2.serialization.MarshalFloat;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 import org.xmlpull.v1.XmlPullParserException;
-
 import android.util.Log;
 
 public class SPTransWS {
 	private final String TAG = "SPTransWS";
 	private final String url;
 	private final String nameSpace = "http://tempuri.org/";
-	private final String metodo = "TracarRota";
-	private final String metodoSOAPAction = "http://tempuri.org/TracarRota";
+	private final String metodo;
+	private final String metodoSOAPAction;
 	
-	public SPTransWS(String url) {
+	public SPTransWS(String url, String metodo) {
 		this.url = url;
+		this.metodo = metodo;
+		this.metodoSOAPAction = "http://tempuri.org/" + metodo;
 	}
 	
-	public String calcularRota(String origem, String destino) throws IOException, XmlPullParserException{
+	public Object retornaPosicaoOnibus(String linha, String onibus){
+
+		try
+		{
+			SoapObject soap = new SoapObject(nameSpace, metodo);
+			soap.addProperty("linha", linha);
+			soap.addProperty("onibus", onibus);
+			SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+			envelope.dotNet = true;
+			envelope.setOutputSoapObject(soap);
+			Log.i(TAG, "Chamando WebService " + url);
+			HttpTransportSE httpTransport = new SPTransHttpTransport(url);
+			httpTransport.debug = true;
+			httpTransport.call(metodoSOAPAction, envelope);
+
+			// Resultado do método do webservice           
+			return envelope.getResponse();                    
+
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			return ex.getMessage();
+		}
+
+	}
+	
+	public String tracarRota(String enderecoOrigem, String enderecoDestino) throws IOException, XmlPullParserException{
 		Object rota;
 		
 		SoapObject soap = new SoapObject(nameSpace, metodo);
-		soap.addProperty("enderecoOrigem", origem);
-		soap.addProperty("enderecoDestino", destino);
+		soap.addProperty("enderecoOrigem", enderecoOrigem);
+		soap.addProperty("enderecoDestino", enderecoDestino);
 		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
 		envelope.dotNet = true;
+		
+	    Marshal floatMarshal = new MarshalFloat();
+	    floatMarshal.register(envelope);
+		
 		envelope.setOutputSoapObject(soap);
 		Log.i(TAG, "Chamando WebService " + url);
 		HttpTransportSE httpTransport = new SPTransHttpTransport(url);
