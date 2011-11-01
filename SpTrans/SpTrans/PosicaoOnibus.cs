030a36b64b3ce4b00ca751b26a4f26d5b669eb24
angelo.rici@gmail.com <angelo.rici@gmail.com@57079b9a-87e6-f1c0-dabe-6aadfb277aba>
2011-11-01 03:01:58 +0000

    WebService para simulação da SpTrans

---
+++ SpTrans/SpTrans/PosicaoOnibus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpTrans
+{
+	public class PosicaoOnibus
+	{
+		public String idPosicaoOnibus { get; set; }
+		public String linha { get; set; }
+		public String onibus { get; set; }
+		public String coordenadaX { get; set; }
+		public String coordenadaY { get; set; }
+
+		public PosicaoOnibus()
+		{
+		}
+	}
+}
